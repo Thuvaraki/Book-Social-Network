45f4b8b4378abec8dec2ceff00c5836a7306575d
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-10-26 23:06:14 +0530

    Configurations

---
+++ src/main/java/com/thuva/book/BookNetworkApiApplication.java
@@ -0,0 +1,14 @@
+package com.thuva.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
