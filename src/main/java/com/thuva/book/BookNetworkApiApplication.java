b717298db61f154ab7e416052eb1c48594c5ad63
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-10-26 23:34:39 +0530

    annotation added to activates the auditing feature

--- src/main/java/com/thuva/book/BookNetworkApiApplication.java
+++ src/main/java/com/thuva/book/BookNetworkApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // a Spring Data JPA annotation that activates the auditing feature.
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
@@ -12,3 +13,10 @@ public class BookNetworkApiApplication {
 	}
 
 }
+
+// @EnableJpaAuditing - a Spring Data JPA annotation that activates the auditing feature.
+//Auditing means Spring can automatically fill fields like:
+		// Who created an entity (@CreatedBy)
+		// When it was created (@CreatedDate)
+		// Who last modified it (@LastModifiedBy)
+		// When it was last modified (@LastModifiedDate)
