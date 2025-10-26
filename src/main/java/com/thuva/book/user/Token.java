2fe7fade3904faae735c5c0bb98d85d430c9100f
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-10-27 00:04:29 +0530

    Token class created

---
+++ src/main/java/com/thuva/book/user/Token.java
@@ -0,0 +1,27 @@
+package com.thuva.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expireAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
