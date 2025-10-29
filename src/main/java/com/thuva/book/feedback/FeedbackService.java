72d41e307393fea4406226603960ab877b92c3ca
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-10-29 20:18:05 +0530

    Implemented Find all feedbacks

--- src/main/java/com/thuva/book/feedback/FeedbackService.java
+++ src/main/java/com/thuva/book/feedback/FeedbackService.java
@@ -2,13 +2,18 @@ package com.thuva.book.feedback;
 
 import com.thuva.book.book.Book;
 import com.thuva.book.book.BookRepository;
+import com.thuva.book.common.PageResponse;
 import com.thuva.book.exception.OperationNotPermittedException;
 import com.thuva.book.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -29,5 +34,23 @@ public class FeedbackService {
         }
         Feedback feedback = feedbackMapper.toFeedback(request);
         return feedbackRepository.save(feedback).getId();
+    }
+
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
+        Pageable pageable = PageRequest.of(page, size);
+        User user = ((User) connectedUser.getPrincipal());
+        Page<Feedback> feedbacks = feedbackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(f -> feedbackMapper.toFeedbackResponse(f, user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
     }
 }
