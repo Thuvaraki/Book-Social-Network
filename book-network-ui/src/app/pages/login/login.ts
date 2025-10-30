4aaaa68dd013342288cfe2b04b988d425fdbd598
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-10-30 15:20:38 +0530

    Implemented Login page

---
+++ book-network-ui/src/app/pages/login/login.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthenticationRequest } from '../../services/models/authentication-request';
+import { AuthenticationService } from '../../services/services/authentication.service';
+import { Router } from '@angular/router';
+import { Token } from '../../services/token/token';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, CommonModule], // FormsModule necessary to use ngModel
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss'],
+})
+export class Login {
+  authRequest: AuthenticationRequest = {
+    email: '',
+    password: '',
+  };
+  errorMsg: Array<String> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    private tokenService: Token
+  ) {}
+
+  login(): void {
+    this.errorMsg = [];
+    this.authService
+      .authenticate({
+        body: this.authRequest,
+      })
+      .then((res) => {
+        this.tokenService.token = res.token as string;
+        // this.router.navigate(['books']);
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.error);
+        }
+      });
+  }
+
+  register(): void {
+    this.router.navigate(['register']);
+  }
+}
